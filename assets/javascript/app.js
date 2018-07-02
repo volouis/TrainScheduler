d5cb941b9b866822266ede9b2740e299f0f4aa53
louisqvo <louisqvo@gmail.com>
2018-07-02 10:36:01 -0400

    added a Delete button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,10 @@ var name;
 var dest;
 var freq;
 var first;
+var rowNum = 0;
+var keys = [];
+
+$(".container").append("<p class=lead style=text-align:center>" + moment().format('LT'));
 
 $("#submit").click(function(event) {
     event.preventDefault();
@@ -22,20 +26,33 @@ $("#submit").click(function(event) {
     console.log(name + dest + freq + first);
 });
 
-
 database.ref().on("child_added", function(snap) {
     var row = $("<tr>");
-    
+    var delBtn = $("<button>");
+
+    delBtn.addClass("btn btn-primary delete");
+    delBtn.text("DELETE");
+    delBtn.attr("type", "button");
+    delBtn.attr("data-row", rowNum);
+
+    row.attr("id", rowNum);   
     row.append("<td>" + snap.val().trainName);
     row.append("<td>" + snap.val().destination);
     row.append("<td>" + snap.val().frequency);
-    row.append("<td>" + arrival(snap.val().trainTime, snap.val().frequency)); //change
-    row.append("<td>" + left(snap.val().trainTime, snap.val().frequency)); //change
+    row.append("<td>" + arrival(snap.val().trainTime, snap.val().frequency)); 
+    row.append("<td>" + left(snap.val().trainTime, snap.val().frequency)); 
+    row.append(delBtn);
 
     $("tbody").append(row);  
-})
+    rowNum++;
+    keys.push(snap.key);
+});
 
-$(".container").append("<p class=lead style=text-align:center>" + moment().format('LT'));
+$(document).on("click","button.delete",function(){
+    var row = $(this).attr("data-row");
+    $("#" + row).text("");
+    database.ref(keys[row]).remove();
+});
 
 function arrival(d, e){
     var l = left(d, e);
@@ -46,16 +63,12 @@ function arrival(d, e){
 function left(a, b){
     var c = moment().format('LT');
     var inter = parseInt(b) * 60;
-    console.log(a + " " + inter + " " + c);
 
     var d =  parseInt(a.substr(0, 2)) * 3600;
     var e =  parseInt(a.substr(3, 2)) * 60;
 
     var start = d + e;
+    var end = moment().unix();;
 
-    var end = moment().unix();
-
-    var timeLeft = Math.ceil((inter - ( end - start ) % inter) / 60);
-
-    return timeLeft;
+    return Math.ceil((inter - ( end - start ) % inter) / 60);
 }
